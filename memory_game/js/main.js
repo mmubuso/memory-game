3dc7db6069e8e1684b8474458c2a50a2f245fe2f
Musiteli Mubuso <mubuso.musiteli@gmail.com>
2019-05-16 23:13:13 -0400

    Game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,12 @@
+// Memory game from GA Pre COurse WOrk
+
+
+//Variables
+var cardOne = "Queen";
+var cardTwo = "King";
+var cardThree = "Queen";
+var cardFour = "King";
+
+//display two cards in console
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardTwo);
