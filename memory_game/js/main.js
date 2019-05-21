de82715ca0a527a804b1f9cf6fa823f2394ea062
Musiteli Mubuso <mubuso.musiteli@gmail.com>
2019-05-21 19:38:39 -0400

    Game Logic for game

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,11 +2,31 @@
 
 
 //Variables
-var cardOne = "Queen";
-var cardTwo = "King";
-var cardThree = "Queen";
-var cardFour = "King";
+// 
+//Variable for holding our set of cards
+var cards = ["queen","queen","king","king"];
+//Varibale for cards in play
+var cardsInPlay = [];
+//Variable for cards flipped
+var cardOne = cards[0];
+var cardTwo = cards[2];
+
+
+//Functions
+// 
+//pushing cards flipped into the cards in play array
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
 
 //display two cards in console
 console.log("User flipped " + cardOne);
 console.log("User flipped " + cardTwo);
+
+//Checks to see if two cards are played and displays appropriate response
+if(cardsInPlay.length === 2){
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match");
+	} else {
+		alert("Sorry, try again");
+	}
+}
