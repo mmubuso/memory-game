5046c9e32b4e8e35266303219a7ba22e3f5ce915
Musiteli Mubuso <mubuso.musiteli@gmail.com>
2019-05-22 21:23:22 -0400

    Revised expressions into resuable functions for flipping a card and checking if two cards match

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -7,26 +7,39 @@
 var cards = ["queen","queen","king","king"];
 //Varibale for cards in play
 var cardsInPlay = [];
-//Variable for cards flipped
-var cardOne = cards[0];
-var cardTwo = cards[2];
-
-
-//Functions
-// 
-//pushing cards flipped into the cards in play array
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-//display two cards in console
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
 
 //Checks to see if two cards are played and displays appropriate response
-if(cardsInPlay.length === 2){
+function checkForMatch(){
+	if(cardsInPlay.length === 2){
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match");
 	} else {
 		alert("Sorry, try again");
 	}
+	}
 }
+
+
+//Functions
+//Function that determines what happens when cards are flipped
+function flipCard(cardId){
+	//pushing cards flipped into the cards in play array
+	cardsInPlay.push(cards[cardId]);
+	//display two cards in console
+	console.log("User flipped " + cards[cardId]);
+	checkForMatch();
+};
+
+
+//Call Functions
+flipCard(0);
+flipCard(2);
+
+//Checks to see if two cards are played and displays appropriate response
+// if(cardsInPlay.length === 2){
+// 	if (cardsInPlay[0] === cardsInPlay[1]) {
+// 		alert("You found a match");
+// 	} else {
+// 		alert("Sorry, try again");
+// 	}
+// }
