aa7efaa93f86876678add46d2e0ef4a79057b367
Musiteli Mubuso <mubuso.musiteli@gmail.com>
2019-05-23 20:23:33 -0400

    Replace items in array with 4 objects

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -4,7 +4,28 @@
 //Variables
 // 
 //Variable for holding our set of cards
-var cards = ["queen","queen","king","king"];
+var cards = [
+	{
+		rank:"queen" ,
+		suit:"hearts" ,
+		cardImage: "images/queen-of-hearts.png"
+	},
+	{
+		rank:"queen" ,
+		suit:"diamonds" ,
+		cardImage: "images/queen-of-diamonds.png"
+	},
+	{
+		rank:"king" ,
+		suit:"hearts" ,
+		cardImage: "images/king-of-hearts.png"
+	},
+	{
+		rank:"king" ,
+		suit:"diamonds" ,
+		cardImage: "images/king-of-diamonds.png"
+	}
+];
 //Varibale for cards in play
 var cardsInPlay = [];
 
@@ -24,9 +45,11 @@ function checkForMatch(){
 //Function that determines what happens when cards are flipped
 function flipCard(cardId){
 	//pushing cards flipped into the cards in play array
-	cardsInPlay.push(cards[cardId]);
+	cardsInPlay.push(cards[cardId].rank);
 	//display two cards in console
-	console.log("User flipped " + cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+	console.log(cards[cardId].suit);
+	console.log(cards[cardId].cardImage);
 	checkForMatch();
 };
 
@@ -35,11 +58,3 @@ function flipCard(cardId){
 flipCard(0);
 flipCard(2);
 
-//Checks to see if two cards are played and displays appropriate response
-// if(cardsInPlay.length === 2){
-// 	if (cardsInPlay[0] === cardsInPlay[1]) {
-// 		alert("You found a match");
-// 	} else {
-// 		alert("Sorry, try again");
-// 	}
-// }
