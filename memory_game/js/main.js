e1ea051c8cbeb79ca00fc4c05783d308512275d1
musiteli mubuso <mubuso.musiteli@gmail.com>
2019-06-15 21:49:50 -0400

    Add screenshot of memory game

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -29,6 +29,19 @@ var cards = [
 //Varibale for cards in play
 var cardsInPlay = [];
 
+
+//Functions
+//Creates the game board we will use for the cards
+function createBoard(){
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src','images/back.png');
+		cardElement.setAttribute('data-id',i);
+
+		cardElement.addEventListener('click',flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+}
 //Checks to see if two cards are played and displays appropriate response
 function checkForMatch(){
 	if(cardsInPlay.length === 2){
@@ -41,9 +54,11 @@ function checkForMatch(){
 }
 
 
-//Functions
+
 //Function that determines what happens when cards are flipped
-function flipCard(cardId){
+function flipCard(){
+	var cardId = this.getAttribute('data-id');
+	this.setAttribute('src',cards[cardId].cardImage);
 	//pushing cards flipped into the cards in play array
 	cardsInPlay.push(cards[cardId].rank);
 	//display two cards in console
@@ -55,6 +70,5 @@ function flipCard(cardId){
 
 
 //Call Functions
-flipCard(0);
-flipCard(2);
+createBoard();
 
